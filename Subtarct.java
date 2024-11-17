85cbb92d0c3441f17a841d1c0851f9eb38f12b60
Subesh Pandey <pandeypaul137@gmail.com>
2024-11-17 09:16:59 -0600

    Subtract

--- Subtarct.java
+++ Subtarct.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
-//Defining the class of java with public access modifier
-public class Add {
-    //Defining the main function of java 
+
+public class Subtarct {
     public static void main(String[]args){
-        //Calling the Scanner class here
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter the positive integer :");
         //Taking the input in java
@@ -13,12 +11,10 @@ public class Add {
         //Taking the input in java
         int b = sc.nextInt();
 
-        int sum =  a+b;
+        int sub =  a-b;
         //Using concatenation to display the input and output in single line
-        System.out.println("The sum of "+a+" and "+b+" is "+sum);
+        System.out.println("The sum of "+a+" and "+b+" is "+sub);
         sc.close();
-    } 
-
-
+    }
+    
 }
-
