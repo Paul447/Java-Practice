cee312d7c1f74e13ec9e07603e9f0d62d2570bd7
Subesh Pandey <pandeypaul137@gmail.com>
2024-11-18 18:36:40 -0600

    StringPalindrome

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
 public class StringPalindrome {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String: ");
+        String s = sc.nextLine();
+        int count = 0;
+        char [] chars = s.toCharArray();
+
+        for(int i =0; i<=chars.length-1;i++){
+            if(chars[i] != chars[chars.length-1-i]){
+                break;
+            }
+            else{
+                count++;
+            }
+        }
+        if(count == chars.length){
+            System.out.println("The character is palindrome");
+        }
+        sc.close();
+    }
 }
